b2273ccbb470dd40d62a2a93940a33385e4245aa
imahnama <muannama25@gmail.com>
2017-09-11 15:18:34 +0300

    added gemfile mail-form

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ gem "font-awesome-rails" # Use jquery as the JavaScript library
 gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
-
+gem 'mail_form', '~> 1.7'
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  resources:contacts, only: [:new, :create]
 root to: 'pages#home'
 
 get "/images/page", to: "pages#images", as: "images"
