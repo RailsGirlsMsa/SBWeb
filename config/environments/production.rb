81b796c39896af7da218f997c688853c249609cf
katherine95 <kathiekim95@gmail.com>
2017-09-21 13:19:26 +0300

    update production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,31 +72,35 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # Use default logging formatter so that PID and timestamp are not suppressed.
+ # Use default logging formatter so that PID and timestamp are not suppressed.
+ # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-config.action_mailer.default_url_options = { host: 'https://swahilibox.herokuapp.com'}
-config.action_mailer.delivery_method = :smtp
-
-ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com',
-  :enable_starttls_auto => true
-}
-end
-
-  # Use a different logger for distributed setups.
-  # require 'syslog/logger'
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
-  end
+# config.log_formatter = ::Logger::Formatter.new
+#
+# config.action_mailer.default_url_options = { host: 'https://swahilibox.herokuapp.com'}
+# config.action_mailer.delivery_method = :smtp
+#
+# ActionMailer::Base.smtp_settings = {
+#   :address => 'smtp.sendgrid.net',
+#   :port => 587,
+#   :authentication => :plain,
+#   :user_name => ENV['SENDGRID_USERNAME'],
+#   :password => ENV['SENDGRID_PASSWORD'],
+#   :domain => 'heroku.com',
+#   :enable_starttls_auto => true
+# }
+# end
+#
+#   # Use a different logger for distributed setups.
+#   # require 'syslog/logger'
+#   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+#
+#   if ENV["RAILS_LOG_TO_STDOUT"].present?
+#     logger           = ActiveSupport::Logger.new(STDOUT)
+#     logger.formatter = config.log_formatter
+#     config.logger = ActiveSupport::TaggedLogging.new(logger)
+#   end
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+end
